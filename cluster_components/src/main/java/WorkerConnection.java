8986f127a8d2bd7be2b4bd414ed3b42634d2e167
Jerryzcn <jerryzh.cn@gmail.com>
2015-12-13 21:19:05 -0800

    workerconnection

--- cluster_components/src/main/java/WorkerConnection.java
+++ cluster_components/src/main/java/WorkerConnection.java
@@ -7,29 +7,44 @@ import java.net.Socket;
 
 public class WorkerConnection implements Runnable {
 
-    private Socket workerSocket;
-    private WorkerInitInfo info;
-    private final float[] dividedData;
+  private Socket workerSocket;
+  private WorkerInitInfo info;
+  private final float[] dividedData;
+  private boolean isStopped;
 
-    public WorkerConnection(Socket workerSocket, WorkerInitInfo info, float[] dividedData) {
-        this.workerSocket = workerSocket;
-        this.info = info;
-        this.dividedData = dividedData;
+  public WorkerConnection(Socket workerSocket, WorkerInitInfo info, float[] dividedData) {
+    this.workerSocket = workerSocket;
+    this.info = info;
+    this.dividedData = dividedData;
+    isStopped = true;
+  }
+
+  @Override public void run() {
+    isStopped = false;
+    try (final BufferedInputStream inBuf = new BufferedInputStream(workerSocket.getInputStream());
+        final BufferedOutputStream outBuf = new BufferedOutputStream(
+            workerSocket.getOutputStream());
+        final ObjectOutputStream out = new ObjectOutputStream(workerSocket.getOutputStream())) {
+
+      out.writeObject(info);
+      out.writeObject(dividedData);
+      out.flush();
+      while (!isStopped()) {
+
+      }
+      outBuf.write(Message.STOP.getBytes("UTF-8"));
+      outBuf.flush();
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void run() {
+  public void stop() {
+    isStopped = true;
+  }
 
-        try (final BufferedInputStream inBuf = new BufferedInputStream(workerSocket.getInputStream());
-             final BufferedOutputStream outBuf = new BufferedOutputStream(workerSocket.getOutputStream());
-            final ObjectOutputStream out = new ObjectOutputStream(workerSocket.getOutputStream())) {
-
-            out.writeObject(info);
-            out.writeObject(dividedData);
-            out.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+  public boolean isStopped() {
+    return isStopped;
+  }
 }
